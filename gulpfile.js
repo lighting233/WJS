6b5f6ed0a99e7c653f5a6146ca0db6be6833e9bd
lighting233 <631649490@qq.com>
2016-10-20 21:51:06 +0800

    '初始版本'

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+
+var gulp = require('gulp');
+
+
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('html',function () {
+	gulp.src('src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('less',function () {
+	gulp.src('src/styles/*.less')
+		.pipe(less())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('dist/styles'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+var uglify = require('gulp-uglify');
+
+gulp.task('script',function () {
+	gulp.src('src/scripts/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/scripts'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('image',function () {
+	gulp.src('src/images/*.*')
+		.pipe(gulp.dest('dist/images'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+var browserSync = require('browser-sync');
+
+// Static server
+gulp.task('serve',function () {
+	browserSync({
+		server: {
+			baseDir: ['dist']
+		}
+	}, function(err, bs) {
+		console.log(bs.options.getIn(["urls", "local"]));
+	});
+
+	gulp.watch('src/*.html',['html']);
+	gulp.watch('src/styles/*.less',['less']);
+	gulp.watch('src/scripts/*.js',['script']);
+	gulp.watch('src/images/*.*',['image']);
+});
---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by light on 2016/10/20.
+ */
+// test123567
+
+$("#header").css('border','1px solid red');
